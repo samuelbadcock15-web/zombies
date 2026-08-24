cd68fcfee6965c33ff87f198dd82e28becbee9ff
samuelbadcock15-web <samuelbadcock15@gmail.com>
2026-08-24 14:53:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 let lobbyState = {
     players: {},
