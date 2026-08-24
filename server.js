3ebfef800eec3115ac99c62f6e0254485d338bda
samuelbadcock15-web <samuelbadcock15@gmail.com>
2026-08-24 15:44:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,17 +1,14 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 
-// Catch-all route to serve the game
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(PORT, () => {
-  console.log(`Zombies game server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
